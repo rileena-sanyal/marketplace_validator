49ff9c7659bbf529465fece9705283010c0690ab
Rileena Sanyal <rileena08@gmail.com>
2025-09-27 19:56:40 +0100

    Update validate_listing_app.py
    updating the base url

--- repo_frontend/validate_listing_app.py
+++ repo_frontend/validate_listing_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://marketplace-validator.onrender.com"
 
 st.title("📝 Validate Your Credit Card Listing")
 
